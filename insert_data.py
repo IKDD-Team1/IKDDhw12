157bb34cae229665d1fa4555bf0957d451fbe9d0
chihsuan, haung <chihsuan.huang.7@gmail.com>
2015-01-12 18:48:44 +0800

    update inser_data.py

--- insert_data.py
+++ insert_data.py
@@ -8,15 +8,20 @@ from modules import csv_io
 from modules import json_io
 from db.MyDB import MyDB
 
-def to_foramt(data, date):
+def to_foramt(data, record):
     new_data = []
     for i in range(1, len(data)):
         row = data[i]
-        if row[-1][:10].replace('-', '') == date:
-            geom = "ST_GeomFromText('POINT(%s %s)', 4326)" % \
+        if row[0] in record:
+            if row[-1] in record[row[0]]:
+                continue
+            else:
+                record[row[0]].append(row[-1])
+        else:
+            record[row[0]] = [row[-1]]
+        geom = "ST_GeomFromText('POINT(%s %s)', 4326)" % \
                                     (row[4].decode('utf-8'), row[5].decode('utf-8'))
-            new_data.append([row[0].decode('utf-8'), geom, row[6].decode('utf-8')])
-
+        new_data.append([row[0].decode('utf-8'), geom, row[6].decode('utf-8'), row[-1].decode('utf-8')])
     return new_data
 
 if __name__=='__main__':
@@ -27,14 +32,15 @@ if __name__=='__main__':
             db_config[u'username'], db_config[u'password'], db_config[u'encoding'], None)
 
     table_name = config[u'table']
+    record = {}
 
     for root, _, files in os.walk(config[u'data'][u'folder_path']):
         for f in files:
             if f != '.DS_Store':
                 dataset = csv_io.read_csv(root+f)
-                data = to_foramt(dataset, f[11:19])
+                data = to_foramt(dataset, record)
                 for item in data:
-                    mydb.insert(table_name, ['siteid', 'geom', 'rainfall10min'], item)
+                    mydb.insert(table_name, ['siteid', 'geom', 'rainfall10min', 'date'], item)
     mydb.close()
                 
 
